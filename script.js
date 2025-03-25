12a3d630753a6ef9fc8630d6928b32add2eb12dc
Robin Gombkoto <robin.gombkoto@gmail.com>
2025-03-25 14:04:56 +0100

    Reveal sections on scroll, added colors & placeholder images

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 // BURGER MENU
+
 const burgerButton = document.getElementById('burger-button');
 const closeButton = document.getElementById('close-button');
 const burgerMenu = document.getElementById('burger-menu');
+const menuItems = burgerMenu.querySelectorAll(".reveal");
 
 burgerButton.addEventListener('click', () => {
-    burgerMenu.classList.remove('hidden')
+    burgerMenu.classList.remove('hidden');
     document.body.style.overflow = 'hidden';
+    
+    menuItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('active');
+        }, index * 100);
+    });
 })
 
 closeButton.addEventListener('click', () => {
     burgerMenu.classList.add('hidden')
     document.body.style.overflow = '';
+    
+    menuItems.forEach((item) => {
+        item.classList.remove('active');
+    });
 })
 
-// LOGO COLOR SWITCH
 
-const header = document.getElementById('header');
-const logoImg = document.getElementById('header-logo');
 
-document.addEventListener('DOMContentLoaded', () => {
-    if(header.classList.contains('bg-dark')){
-        logoImg.src = "./images/tpc-light.svg";
+// REVEAL SECTIONS ON SCROLL
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+
+    const sectionObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                revealElements(entry.target);
+            }
+        });
+    }, { threshold: 0.3 });
+
+    sections.forEach((section) => {
+        sectionObserver.observe(section);
+    });
+
+    function revealElements(section) {
+        const reveals = section.querySelectorAll(".reveal");
+
+        reveals.forEach((reveal, index) => {
+            setTimeout(() => {
+                reveal.classList.add("active");
+            }, index * 600);
+        });
     }
-    else{
-        logoImg.src = "./images/tpc-dark.svg";
-    }
-})
-
+});
